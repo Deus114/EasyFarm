ca71a51fde5f226ee9d5054a7950c63221eac257
KhoaTranAnh12 <k245416587@gmail.com>
2025-04-24 14:26:23 +0700

    nativewind config

--- App.jsx
+++ App.jsx
@@ -6,7 +6,7 @@ import HomeScreen from './screens/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/auth/LoginScreen';
-
+import "./global.css"
 const Stack = createNativeStackNavigator();
 
 export default function App() {
